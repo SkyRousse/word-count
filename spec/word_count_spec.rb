50537ad69fe495ce2c9788be8876b0c44fd2ee7a
sr <skyrousse@gmail.com>
2016-07-01 09:13:55 -0700

    add two additional passing specs

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,6 +2,12 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
+  it('compares two words to see if they are the same') do
+    expect('Boom'.word_count('Boom')).to(eq(1))
+  end
+  it('compares two words to see if they are the same word') do
+    expect('babble'.word_count('latin')).to(eq(0))
+  end
   it('finds one instance of a target word in a string input that contains multiple words') do
     expect('mic drop leatherpants lamborgini mercy'.word_count("leatherpants")).to(eq(1))
   end
