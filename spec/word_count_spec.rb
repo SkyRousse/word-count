1afe9ca8f258a6794d95328f1fb73e1f1948b9eb
sr <skyrousse@gmail.com>
2016-07-01 09:06:59 -0700

    add second passing spec

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it('finds the frequency of a word input in a string input') do
-    expect('Little hands Little Marco rich tremendous what spray-can orange eeeyuuuuge'.word_count("Little")).to(eq(2))
+  it('finds one instance of a target word in a string input that contains multiple words') do
+    expect('mic drop leatherpants lamborgini mercy'.word_count("leatherpants")).to(eq(1))
+  end
+  it('finds more than one instance of a target word in a string input that contains multiple words') do
+  expect('Little hands Little Marco rich tremendous what spray-can orange eeeyuuuuge'.word_count("Little")).to(eq(2))
   end
 end
